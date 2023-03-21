a904d9b101b9a9c9435b0cba764e905b3ad36aae
kamy98 <kamilysimeao@gmail.com>
2023-03-20 22:46:38 -0300

    ETEC CONTAS 2003

--- Aula1303/index.js
+++ Aula1303/index.js
@@ -6,17 +6,18 @@ const inquirer = require('inquirer')
 
 //#region Modulos Internos
 const fs = require('fs')
+const {console} = require ('console')
 //#endregion
 
 operation()
 //#region Operaçoes Inicias
-function operation(){
+function operation() {
     inquirer.prompt([
         {
-            type:'list',
+            type: 'list',
             name: 'action',
             message: 'O que deseja Fazer',
-            choices:[
+            choices: [
                 'Crian Conta',
                 'Consultar Saldo',
                 'Depositar',
@@ -24,31 +25,69 @@ function operation(){
                 'Sair'
             ]
         }
-    ]).then((answer) =>{
+    ]).then((answer) => {
         const action = answer['action']
 
-        if(action === 'Criar Conta'){
+        if (action === 'Criar Conta') {
             console.log('Criando Sua Conta')
+            createAccount()
 
-        }else if(action === 'Consultar Saldo'){
+        } else if (action === 'Consultar Saldo') {
             console.log("Consultando Saldo")
 
-        }else if(action === 'Depositar'){
+        } else if (action === 'Depositar') {
             console.log('Depositando')
 
-        }else if(action === 'Sacar'){
+        } else if (action === 'Sacar') {
             console.log('Sacando')
 
-        }else if(action === 'Sair'){
+        } else if (action === 'Sair') {
             console.log(chalk.bgBlue.white('Obrigado Por Ultilizar O Contas ETEC'))
             setTimeout(() => {
                 process.exit()
             }, 1500);
         }
-
-
-
-
     })
 }
 //#endregion
+
+//#region criacao de contas
+function createAccount() {
+    console.log(chalk.bgBlue.white("parabens por escolher esse banco"))
+    console.log(chalk.green("escolha as opçoes de conta"))
+
+    buildAccount()
+
+}
+function buildAccount() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'entre com nome da sua conta'
+        }
+    ]).then((answer) => {
+        console.info(answer['accountName'])
+        const accountName = answer['accountName']
+        if (!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts')
+        }
+        if (fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(
+                chalk.bgCyan.black('esta conta ja existe!')
+            )
+            buildAccount()
+        }
+        fs.writeFileSync(
+            `accounts/${accountName}.json`,
+            '("balance: 0")',
+            function (err) {
+                console.error(err)
+            }
+
+        )
+
+        console.info(chalk.green("Parabens, sua conta esta pronta"))
+        operation()
+    })
+}
+//#endregion 
