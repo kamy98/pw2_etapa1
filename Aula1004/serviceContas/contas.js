1e48bbaa2fd097658c97a7dc38720b43cf73a112
kamy98 <kamilysimeao@gmail.com>
2023-04-13 20:37:48 -0300

    Aula 1304 Exercicio de Modulação Etec Contas

--- Aula1004/serviceContas/contas.js
+++ Aula1004/serviceContas/contas.js
@@ -79,5 +79,152 @@ module.exports = {
             console.info(chalk.green('Parabéns, sua conta está pronta!'))
             this.operation()
         })
-    }
+    },
+
+     deposit(){
+        inquirer.prompt([
+            {
+                name:'accountName',
+                message:'Para qual conta irá o depósito?'
+            }
+        ]).then((answer) => {
+            const accountName =answer['accountName']
+            if(!this.checkAccount(accountName)){
+                return this.deposit()
+            }
+    
+            inquirer.prompt([
+                {
+                    name:'amount',
+                    message: 'Quanto você deseja depositar?'
+                }
+            ]).then((answer) =>{
+                const amount = answer['amount']
+                this.addAmount(accountName, amount)
+                console.log(chalk.bgYellow.green('Sucesso! Seu montante foi depositado.'))
+                setTimeout(() => {
+                    this.operation()
+                }, 1000);
+            })
+        })
+    },
+
+    checkAccount(accountName){
+        if(!fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black('Esta conta não existe.'))
+            return false
+        }
+        return true
+    },
+
+     addAmount(accountName, amount){
+        const accountData = this.getAccount(accountName)
+    
+        if(!amount){
+            console.log(chalk.bgRed.black('Erro de montante!'))
+            return this.deposit()
+        }
+    
+        accountData.balance = parseFloat(amount)+parseFloat(accountData.balance)
+    
+        fs.writeFileSync(
+            `accounts/${accountName}.json`,
+            JSON.stringify(accountData),
+            function (err){
+                console.log(err)
+            }
+        )
+        console.log(chalk.green('Seu valor foi depositado!'))
+    },
+
+     getAccount(accountName){
+        const accountJSON = fs.readFileSync(`accounts/${accountName}.json`,
+        {
+            encoding:'utf8',
+            flag:'r'
+        })
+        return JSON.parse(accountJSON)
+    },
+
+    accountBalance(){
+        inquirer.prompt([
+            {
+                name: 'accountName',
+                message: 'Qual conta deseja verificar o saldo?'
+            }
+        ]).then((answer) =>{
+            const accountName = answer['accountName']
+    
+            if(!this.checkAccount(accountName)){
+                return this.accountBalance()
+            }
+            const accountData = this.getAccount(accountName)
+            if(accountData.balance>0){
+                console.log(chalk.green(`O saldo da conta: R$ ${accountData.balance}.`))
+            }else{
+                console.log(chalk.red(`O saldo da conta: R$ ${accountData.balance}.`))
+            }
+            setTimeout(() => {
+               this.operation() 
+            }, 1000)
+        })
+    },
+
+    withdraw(){
+        inquirer.prompt([
+            {
+                name:'accountName',
+                message: 'Qual conta efetuará o saque?'
+            }
+        ]).then((answer) => {
+            const accountName = answer['accountName']
+    
+            if(!this.checkAccount(accountName)){
+                return this.withdraw()
+            }
+    
+            inquirer.prompt([
+                {
+                    name: 'amount',
+                    message: 'Quanto deseja sacar?'
+                }
+            ]).then((answer) =>{
+                const amount = answer['amount']
+    
+                this.removeAmount(accountName, amount)
+                this.operation()
+            })
+        })
+    },
+
+     removeAmount(accountName, amount){
+        const accountData = this.getAccount(accountName)
+    
+        if(!amount){
+            console.log(chalk.bgRed.black('O valor precisa ser informado!'))
+            return this.withdraw()
+        }
+        
+        if(accountData.balance < amount){
+            console.log(chalk.bgYellow.black('Entrou no cheque especial!'))
+        }
+    
+        if((accountData.balance + accountData.limit)<amount){
+            console.log(chalk.bgRed.black('Não há limite na conta!'))
+            return
+        }else{
+            accountData.balance = parseFloat(accountData.balance) - parseFloat(amount)
+    
+            fs.writeFileSync(
+                `accounts/${accountName}.json`,
+                JSON.stringify(accountData),
+                function (err){
+                    console.log(err)
+                }
+            )
+        
+            console.log(chalk.green(`Foi sacado: ${amount} da conta ${accountName}.`))
+        }
+    },
+
 }
